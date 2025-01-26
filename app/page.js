702dd13b295ab0a85f5596f7665939ae99a3bbc2
GemboJones <gembojones@gmail.com>
2025-01-26 23:35:55 +0000

    homepage update

--- app/page.js
+++ app/page.js
@@ -1,12 +1,9 @@
 import { Button } from "@/components/ui/button";
-import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div>
       <h2>hi</h2>
-      <Button>Click me</Button>
-      <UserButton />
     </div>
   );
 }
